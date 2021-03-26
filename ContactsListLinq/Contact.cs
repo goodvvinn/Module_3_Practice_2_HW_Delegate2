4e5bb1b6c26d531d74dcde2bd1d5ce5c5e2e052b
goodvvinn <goodvvinn@gmail.com>
2021-03-26 14:44:32 +0200

    Add files via upload

---
+++ ContactsListLinq/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsListLinq
+{
+    public class Contact
+    {
+        public Contact()
+        {
+        }
+
+        public Contact(string firstName, string lastName, int age, string phoneNum)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            PhoneNum = phoneNum;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string PhoneNum { get; set; }
+        public void Display()
+        {
+            Console.WriteLine($"|First name: {FirstName}, |Last name: {LastName}, |age: {Age}, |phone number: {PhoneNum}");
+        }
+    }
+}
---
+++ ContactsListLinq/ContactsList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsListLinq
+{
+    public class ContactsList
+    {
+        public ContactsList()
+        {
+            CList = new List<Contact>()
+            {
+                new Contact("Alex", "Goodvvinn", 37, "+38066-361-87-93"),
+                new Contact("Bob", "Marley", 100, "+0123-456-789"),
+                new Contact("John", "Doe", 150, "+987-765-54"),
+                new Contact("Alice", "Cooper", 70, "+6987-345-23"),
+                new Contact("Bill", "Jones", 50, "+345-44-22"),
+            };
+        }
+
+        public List<Contact> CList { get; set; }
+    }
+}
