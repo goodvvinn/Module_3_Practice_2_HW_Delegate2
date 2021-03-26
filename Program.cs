58ca1b30a703404076afc726f9bf9fa45879b9fc
goodvvinn <goodvvinn@gmail.com>
2021-03-26 14:36:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExampleDelegate2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var start = new Starter();
+            start.GeneralSum();
+        }
+    }
+}
---
+++ Starter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ExampleDelegate2
+{
+	public class Starter
+	{
+		public event Func<double, double, double> SumHandler;
+
+		public double GeneralSum()
+		{
+			SumHandler += Sum;
+			SumHandler += Sum;
+
+			double result = 0;
+			TryCatchMethod(() =>
+			{
+				foreach (var item in SumHandler.GetInvocationList())
+				{
+					result += (double)item.DynamicInvoke(3, 5);
+				}
+			});
+			Console.WriteLine(result);
+			return result;
+		}
+
+		public void TryCatchMethod(Action action)
+		{
+			try
+			{
+				action();
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+
+		private double Sum(double x, double y)
+		{
+			var res = x + y;
+			Console.WriteLine(res);
+			return res;
+		}
+	}
+}
